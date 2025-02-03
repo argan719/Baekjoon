5935a8a8d92c8d120653b880d42e62cf2ec187f7
argan719 <64789601+argan719@users.noreply.github.com>
2025-02-03 15:11:06 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    long long n;
+    cin >> n;
+    
+    cout << (n * (n-2) * (n-1))/6 << "\n" << 3;
+}
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
