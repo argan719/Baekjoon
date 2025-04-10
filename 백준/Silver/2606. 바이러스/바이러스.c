9523cc2e9f82b29eeb4af1704c2558b442b05e2a
argan719 <64789601+argan719@users.noreply.github.com>
2025-04-10 09:01:08 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.c
+++ 백준/Silver/2606. 바이러스/바이러스.c
@@ -1,35 +1,67 @@
 #include<stdio.h>
-#define MAX 102
+#include<stdlib.h>
 
-int N, C;
-int matrix[MAX][MAX];
-int visited[MAX];
-
+int N, E;
+int** matrix;  // int* x N개
+int* visit;
+int* size;
 int cnt;
-void AddEdge(int f, int t) {
-	matrix[f][t] = 1;
-	matrix[t][f] = 1;
+
+void makeList(int n1, int n2) {
+
+	matrix[n1][size[n1]++] = n2;
+	matrix[n2][size[n2]++] = n1;
+
+	matrix[n1] = realloc(matrix[n1], (size[n1] + 1) * sizeof(int));
+	matrix[n2] = realloc(matrix[n2], (size[n2] + 1) * sizeof(int));
 }
 
 void DFS(int v) {
-	visited[v] = 1;
-	for (int i = 1; i <= C; i++) {
-		if (!visited[i] && matrix[v][i]) {
+	visit[v] = 1;
+	for (int i = 0; i < size[v]; i++) {
+		if (!visit[matrix[v][i]]) {
 			cnt++;
-			DFS(i);
+			DFS(matrix[v][i]);
 		}
 	}
 }
 
-int main(void) {
-	scanf("%d", &C);
-	scanf("%d", &N);
+void init(void) {
+	matrix = (int**)calloc(N, sizeof(int*));
+	visit = (int*)malloc(N * sizeof(int));
+	memset(visit, 0, N * sizeof(int));
+	size = (int*)calloc(N, sizeof(int));
+	//memset(size, 0, C * sizeof(int));
 
-	int f, t;
-	for (int i = 0; i < N; i++) {
-		scanf("%d %d", &f, &t);
-		AddEdge(f, t);
+	if (!matrix || !visit || !size) {
+		exit(0);
 	}
-	DFS(1);
+	for (int i = 0; i < N; i++) {
+		matrix[i] = (int*)calloc(1, sizeof(int));
+		size[i] = 0;
+	}
+}
+
+void destroy(void) {
+	free(visit);
+	free(size);
+	for (int i = 0; i < N; i++) {
+		free(matrix[i]);
+	}
+	free(matrix);
+}
+
+int main(void) {
+	scanf("%d", &N);
+	scanf("%d", &E);
+
+	init();
+	int f, t;
+	for (int i = 0; i < E; i++) {
+		scanf("%d %d", &f, &t);
+		makeList(f - 1, t - 1); // 정점번호를 0 ~ N-1
+	}
+	DFS(0);
 	printf("%d", cnt);
+	destroy();
 }
