b8013931634bb8634c1ce658960f53585eb65bd5
argan719 <64789601+argan719@users.noreply.github.com>
2025-04-24 07:45:40 +0900

    Title: Date to Date, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250424/Date to Date/date-to-date.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int m1, d1, m2, d2;
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    // Please write your code here.
+    int arr[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for(int i=1; i<=m2; i++){
+        sum2 += arr[i];
+    }
+    sum2 += d2;
+
+    for(int i=1; i<=m1; i++){
+        sum1 += arr[i];
+    }
+    sum1 += d1;
+
+    cout << sum2 - sum1;
+
+    return 0;
+}
