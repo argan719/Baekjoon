d8c09c7c02a82ca0ab9405859e58ce57002d6dd4
argan719 <64789601+argan719@users.noreply.github.com>
2025-05-12 08:46:58 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 8ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250512/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 250512/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -26,8 +26,7 @@ int main() {
     for(int i=1; i<3; i++){
         for(int x = x1[i]; x<x2[i]; x++){
             for(int y=y1[i]; y<y2[i]; y++){
-                if(matrix[x][y] == 1 && i == 2) matrix[x][y] = 3;  // 겹치는 부분
-                else matrix[x][y] = i;  // 1 2
+                matrix[x][y] = i;  
             }
         }
     }
@@ -39,6 +38,7 @@ int main() {
     int max_y = INT_MIN;
 
     int cnt = 0;
+    int flag = 0;
     for(int i=x1[1]; i<x2[1]; i++){
         for(int k=y1[1]; k<y2[1]; k++){
             if(matrix[i][k]) {
@@ -46,11 +46,13 @@ int main() {
                 if(min_y > k) min_y = k;
                 if(max_x < i) max_x = i;
                 if(max_y < k) max_y = k;
+                flag = 1;
             }
         }
     }
 
-    cout << (max_x - min_x + 1) * (max_y - min_y + 1);
+    if(!flag) cout << 0;
+    else cout << (max_x - min_x + 1) * (max_y - min_y + 1);
 
     return 0;
 }
