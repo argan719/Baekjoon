cd64201fc9da82805e4e2819559db2a6c986c2e0
argan719 <64789601+argan719@users.noreply.github.com>
2025-05-14 08:14:44 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250514/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define MAX 1001
+using namespace std;
+
+int arr[MAX];
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+
+    int cnt = 1;
+    int max = 1;
+    for(int i=1; i<N;i++){
+        if(arr[i-1] < arr[i]) {
+            cnt++;
+            if(max < cnt) max = cnt;
+        }
+        else cnt = 1;
+    }
+
+    cout << max;
+
+    return 0;
+}
