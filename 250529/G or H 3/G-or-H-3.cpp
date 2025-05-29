88ef225105eb9ad2ea1bab0be2243f4d762efac7
argan719 <64789601+argan719@users.noreply.github.com>
2025-05-29 09:12:11 +0900

    Title: G or H 3, Time: 19ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250529/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<algorithm>
+#include<climits>
+#define MAX 10001
+using namespace std;
+
+int arr[MAX];
+int main() {
+    // Please write your code here.
+    int N, K;
+    cin >> N >> K;
+
+    int pos;
+    char val;
+    int max_pos = INT_MIN;
+    int max = INT_MIN;
+    int sum = 0;
+
+    for(int i=0;i<N;i++){
+        cin >> pos >> val;
+        if(val == 'G') arr[pos] = 1;
+        else if(val == 'H') arr[pos] = 2;
+        if(pos > max_pos) max_pos = pos;
+        sum += arr[pos];
+    }
+
+    if(max_pos < K) {
+        cout << sum; exit(0);
+    }
+
+    sum = 0;
+    for(int i=1;i<=max_pos - K; i++){
+        sum = 0;
+        for(int k=i; k<= i+K; k++){
+            sum += arr[k];
+        }
+        if(max < sum) max = sum;
+    }
+
+    cout << max;
+
+    return 0;
+}
