651b7c3dd784f8f41a310a3a0c884801dedeb41f
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-11 08:29:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,46 +1,43 @@
 #include<iostream>
+#include<vector>
 #include<queue>
-#include<map>
+#include<cstring>
+#include<climits>
 #define MAX 100001
 using namespace std;
 
 int visited[MAX];
 int N, K;
 
-void bfs(){
+int bfs(){
     queue<int> q;
-    map<int, int> depth;
     q.push(N);
     visited[N] = 1;
-    depth[N] = 0; // 시작 노드 깊이 설정
+    int step = 0;
     
     while(!q.empty()){
-        auto a = q.front();
-        q.pop();
+        int len = q.size();
         
-        if(a == K) {
-            cout << depth[a];
-            return;
-        }
+        for(int i=0; i<len; i++){
+            auto cur = q.front();
+            q.pop();
         
-        if(!visited[a-1] && 0<= a-1 && a-1 <= MAX) {
-            q.push(a-1);
-            visited[a-1] = 1;
-            depth[a-1] = depth[a] + 1;
+            if(cur == K){
+                cout << step;
+                return 0;
+            }
+            
+            for(auto n : {cur-1, cur+1, cur*2}){
+                if(n < 0 || n > MAX-1) continue;
+                if(visited[n]) continue;
+                visited[n] = 1;
+                q.push(n);
+            }
         }
-        if(!visited[a+1] && 0<= a+1 && a+1 <= MAX) {
-            q.push(a+1);
-            visited[a+1] = 1;
-            depth[a+1] = depth[a] + 1;
-        }
-        if(!visited[a*2] && 0<= a*2 && a*2 <= MAX) {
-            q.push(2*a);
-            visited[2 * a] = 1;
-            depth[2 * a] = depth[a] + 1;
-        } 
+        step++;
     }
+    return -1;
 }
-
 int main(void){
     cin >> N >> K;
     bfs();
