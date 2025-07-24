0d70ddbc14ff8915cbadbd8397657348e1f4e81e
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-24 11:15:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 100 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,25 +1,34 @@
-// 벡터 pair 로 다시 풀기
-
 #include<iostream>
-#include<vector>
 #include<algorithm>
 #define MAX 100001
 using namespace std;
 
 int N;
-//vector<pair<int,int>> vec(MAX);
-vector<pair<int,int>> vec;
+//int arr[MAX][2]; // xi, yi
+typedef struct p{
+    int x;
+    int y;
+}pos;
+pos arr[MAX]; // 구조체 배열로 변경
+
+bool comp(const pos &a, const pos &b){ // const & 붙이는 버릇!
+    if(a.x < b.x) return true;  // 복사 안함, 변경도 안함 (그냥 a 보다 훨씬 효율적)
+    if(a.x == b.x) {
+        if(a.y <= b.y) return true;
+        else return false;
+    }
+    return false;
+}
 
 int main(void){
     cin >> N;
-    int first, second;
     for(int i=0; i<N; i++){
-        cin >> first >> second;
-        vec.push_back(make_pair(first, second));
+        cin >> arr[i].x >> arr[i].y;
     }
-    sort(vec.begin(), vec.end());
     
-    for(auto cur: vec){
-        cout << cur.first << " " << cur.second << "\n";
+    sort(arr, arr+N, comp);
+    
+    for(int i=0; i<N; i++){
+        cout << arr[i].x << " " << arr[i].y << "\n";
     }
 }
