36bd12401d6801f439e0370b62d8cefac46da8f7
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-29 22:04:46 +0900

    [Silver V] Title: 투명, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/1531. 투명/투명.cc
+++ 백준/Silver/1531. 투명/투명.cc
@@ -1,32 +1,26 @@
 #include<iostream>
-#define MAX 101
 using namespace std;
 
-int matrix[MAX][MAX];
-int arr[50][4]; // left x, y, right x, y
-
+int matrix[101][101];
+int N, M;
 int main(void){
-    int N, M;
-    int cnt = 0;
-    
     cin >> N >> M;
-    for(int i=0; i<N;i++){
-        // r c x y
-        cin >> arr[i][0] >> arr[i][1] >> arr[i][2] >> arr[i][3];
+    int cnt = 0;
+    int x1, y1, x2, y2;
+    for(int i=0; i<N; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
         
-        for(int r=arr[i][0]; r<=arr[i][2]; r++){
-            for(int c = arr[i][1]; c<=arr[i][3]; c++){
+        for(int r=x1; r<=x2; r++){
+            for(int c=y1; c<=y2; c++){
                 matrix[r][c]++;
             }
         }
     }
     
-    for(int i=1; i<=100;i++){
+    for(int i=1; i<=100; i++){
         for(int j=1; j<=100; j++){
             if(matrix[i][j] > M) cnt++;
         }
     }
     cout << cnt;
-    
-   
 }
