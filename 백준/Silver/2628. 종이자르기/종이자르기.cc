7ac8fd8bd773bae62a3848ac570d857d020732ef
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-29 23:12:59 +0900

    [Silver V] Title: 종이자르기, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.cc
+++ 백준/Silver/2628. 종이자르기/종이자르기.cc
@@ -11,52 +11,42 @@ int main(void){
     cin >> w >> h;
     cin >> N;
     
-    /*
-    int dimension = w * h; // 전체면적
-    int max_rectangle = 0;
-    int w_line_top = 0; // 가로선
-    int w_line_bottom = h;
-    int h_line_top = 0; // 세로선 left
-    int h_line_bottom =  w; // 세로선 right
-     */
-    // 그냥 top, bottom, left, right으로 할 걸..
-    
     int id, no;
     int max_w = -1;
     int max_h = -1;
-    // no를 입력받아 자른다는 것은 -> no : tot-no
-    // 전부 잘라놓고 맨 마지막에 넓이 계산 ? -> (처음에 생각한)이게 맞는듯.
-    // 자르는 것은 모든 분할 사각형에 해당됨. 즉, 계속 max인 면적만 확인하면 됨. -> no 반례 존재. 그래서 틀림.
-    
-    vector<int> w_id;
-    vector<int> h_id;
-    for(int n=1; n<=N; n++){
-        cin >> id >> no;
-        if(id ==0){//가로
-            h_id.push_back(no);
-        }else{//세로
-            w_id.push_back(no);
-        }
-    }
-    
-    sort(w_id.begin(), w_id.end());
-    sort(h_id.begin(), h_id.end());
     int tmp;
     int prev = 0;
-    for(auto cur : w_id){
-        tmp = cur - prev;
-        prev = cur;
-        if(max_w < tmp) max_w= tmp;
-    }
-    if(max_w < w - prev) max_w = w-prev;
     
-    prev = 0;
-    for(auto cur : h_id){
+    vector<int> row; // 행번호 = 가로 점선
+    vector<int> col; // 열번호 = 세로 점선
+    for(int n=1; n<=N; n++){
+        cin >> id >> no;
+        if(id ==0){//가로로 자르는 점선
+            row.push_back(no);
+        }else{//세로로 자르는 점선
+            col.push_back(no);
+        }
+    }
+    row.push_back(h);
+    col.push_back(w);
+    
+    sort(row.begin(), row.end());
+    sort(col.begin(), col.end());
+    
+    for(auto cur : row){
         tmp = cur - prev;
         prev = cur;
         if(max_h < tmp) max_h= tmp;
     }
-    if(max_h < h - prev) max_h = h-prev;
+    //if(max_w < w - prev) max_w = w-prev;
+    
+    prev = 0;
+    for(auto cur : col){
+        tmp = cur - prev;
+        prev = cur;
+        if(max_w < tmp) max_w = tmp;
+    }
+    //if(max_h < h - prev) max_h = h-prev;
     
     cout << max_h * max_w;
     
