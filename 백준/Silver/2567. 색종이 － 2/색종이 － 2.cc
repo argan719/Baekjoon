376df088c55f6253651ae24a655160d03cffd324
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-30 22:35:37 +0900

    [Silver IV] Title: 색종이 - 2, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.cc
@@ -0,0 +1,39 @@
+#include<iostream>
+#define MAX 101
+using namespace std;
+
+int matrix[MAX][MAX];
+int N;
+
+int dr[] = {-1, 1, 0, 0};
+int dc[] = {0, 0, -1, 1};
+int main(void){
+    cin >> N;
+    int x, y;
+    int nr, nc;
+    int cnt = 0;
+    for(int i=0; i<N; i++){
+        cin >> x >> y;
+        
+        for(int r=x; r < x+ 10; r++){
+            for(int c=y; c < y + 10; c++){
+                matrix[r][c] = 1;
+            }
+        }
+    }
+    
+    for(int i=0; i<=99; i++){
+        for(int j=0; j<= 99; j++){
+            if(matrix[i][j] == 0) continue;
+            
+            for(int k=0;k<4; k++){
+                nr = i + dr[k];
+                nc = j + dc[k];
+                //if(nr >=0 && nc >=0 && nr <=99 && nc <=99)
+                if(matrix[nr][nc] == 0) cnt++;
+            }
+        }
+    }
+    cout << cnt;
+    
+}
