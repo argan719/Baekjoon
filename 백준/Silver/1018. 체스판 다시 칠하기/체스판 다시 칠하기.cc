d7135f77ba07f8233dd2256a7c4a6dfb6b110b83
argan719 <64789601+argan719@users.noreply.github.com>
2025-07-31 22:58:22 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,50 +1,38 @@
 #include<iostream>
-#include<cstring>
 #include<algorithm>
+#include<cstring>
 #define MAX 51
 using namespace std;
 
-string matrix[MAX];
 int N, M;
-
-char flag = 'A';
-
-int solve(int i, int j){
-    int cnt = 0;
-    
-    for(int r=i; r<i+8; r++){
-        for(int c=j; c<j+8; c++){
-            if((r-i)%2 == 0){
-                if((c-j)%2 == 0 && matrix[r][c] == 'W') cnt++;
-                else if((c-j)%2 == 1 && matrix[r][c] == 'B') cnt++;
-            }
-            else {
-                if((c-j)%2 == 0 && matrix[r][c] == 'B') cnt++;
-                else if((c-j)%2 == 1 && matrix[r][c] == 'W') cnt++;
-            }
-        }
-    }
-    return cnt;
-}
-
-
+char matrix[MAX][MAX];
 
 int main(void){
     cin >> N >> M;
-    int result = 100;
+    string str;
+    int cnt = 0;
+    int min_val = 300;
     
     for(int i=0; i<N; i++){
-        cin >> matrix[i];
-    }
-    // 8x8 떼어내기
-    for(int i=0; i<=N-8; i++){
-        for(int j=0; j<=M-8; j++){ 
-            flag = 'B';
-            int cnt = solve(i,j);
-            cnt = min(cnt, 64-cnt);
-            result = min(result, cnt); 
-            //flag = 'W';
+        cin >> str;
+        for(int j=0; j<M; j++){
+            matrix[i][j] = str[j];
         }
     }
-    cout << result;
+    
+    for(int r=0; r<= N-8; r++){
+        for(int c=0; c<=M-8; c++){
+            cnt = 0;
+            for(int i=r; i<r+8; i++){
+                for(int j=c; j<c+8; j++){
+                    if((i+j)%2 == 0 && matrix[i][j] != 'W') cnt++;
+                    else if((i+j)%2 == 1 && matrix[i][j] != 'B') cnt++;
+                }
+            }  // 2가지 경우에 대해서만 고려
+            // 맨 왼 앞이 W인 체스판일 때 색칠해야 할 개수가 적은지,
+            // 맨 왼 앞이 B인 체스판일 때 색칠해야 할 개수가 적은지 비교
+            min_val = min(min_val, min(cnt, 64-cnt));
+        }
+    }
+    cout << min_val;
 }
