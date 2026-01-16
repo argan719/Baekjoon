5d69a8d320d1061f7033ac37dbd2d565162772f4
argan719 <64789601+argan719@users.noreply.github.com>
2026-01-16 17:39:50 +0900

    [Silver II] Title: DFS와 BFS, Time: 8 ms, Memory: 5944 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,71 +1,79 @@
 #include<iostream>
 #include<queue>
-#include<cstring> // memset
+#include<cstring>
 #include<vector>
 #define MAX 1001
+
 using namespace std;
 
 int N, M, V;
 int matrix[MAX][MAX];
 int visited[MAX];
-vector<int> dfs_result;
-vector<int> bfs_result;
 
-void dfs(int v){
-    visited[v] = 1;  // 방문 표시
-    dfs_result.push_back(v); // 방문 결과 삽입
-    
-    for(int i=1; i<=N; i++){  // 인접하고, 미방문이면 dfs 호출
-        if(matrix[v][i] && !visited[i]) dfs(i);
-    }
-}
+vector<int> bfs_result;
+vector<int> dfs_result;
 
 void bfs(){
-    queue<int> q;
-    q.push(V); // 시작 정점 삽입
+    queue<int> q;   // [1]
+    q.push(V);  // [2] 시작정점 삽입
     visited[V] = 1;
     
-    while(!q.empty()){
+    
+    while(!q.empty()) {  // [3] queue에 데이터 있는 동안
         // 맨 앞 요소 리턴
         auto front = q.front();
-        // 삭제 (pop은 반환값 없음)
-        q.pop();
+        // 삭제 - pop은 리턴값 없음
+        q.pop();        // 데이터 한 개 꺼냄
         // 방문 결과 삽입
         bfs_result.push_back(front);
         
-        for(int i=1; i<=N; i++){
-            if(matrix[front][i] && !visited[i]){
-                q.push(i);
+        // 정점 번호는 1부터 N까지이다.
+        for(int i=1; i<=N; i++){  // 연결된 노드 or 방향 반복처리
+            if(matrix[front][i] && !visited[i]) {  // 범위 내 + 미방문 + (조건 맞으면)
+                q.push(i);       // 단위작업 (해야할 일)
                 visited[i] = 1;
             }
         }
     }
+    
+    
 }
 
-int main(void){
+void dfs(int v){
+    visited[v] = 1;  // 방문표시
+    dfs_result.push_back(v);
+    
+    // 정점 번호는 1부터 N까지이다.
+    for(int i=1; i<=N; i++){
+        if(matrix[v][i] && !visited[i]) dfs(i);  // 인접하고 미방문이면 재귀호출
+    }
+}
+
+int main(void)
+{
     cin >> N >> M >> V;
     
-    int a, b;
+    int r, c;
+    // 양방향 인접행렬 생성
     for(int i=0; i<M; i++){
-        cin >> a >> b;
-        // 양방향 인접행렬 생성
-        matrix[a][b] = 1;
-        matrix[b][a] = 1;
+        cin >> r >> c;
+        matrix[r][c] = 1;
+        matrix[c][r] = 1;
     }
     
     dfs(V);
     // visited 배열 초기화
     memset(visited, 0, sizeof(int)*MAX);
+    
     bfs();
     
-    // dfs 결과 출력
-    for(auto it = dfs_result.begin(); it != dfs_result.end(); it++){
-        cout << *it << " ";
+    // DFS 결과 출력
+    for(auto cur : dfs_result) {
+        cout << cur << " ";
     }
     cout << endl;
-    // bfs 결과 출력
-    for(auto it = bfs_result.begin(); it != bfs_result.end(); it++){
-        cout << *it << " ";
+    // BFS 결과 출력
+    for(auto cur : bfs_result) {
+        cout << cur << " ";
     }
-    
 }
