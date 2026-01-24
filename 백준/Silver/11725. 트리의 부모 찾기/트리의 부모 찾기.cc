8631637d4cbbd9d9f2cd9676b23dea1bb0f1edc6
argan719 <64789601+argan719@users.noreply.github.com>
2026-01-24 15:49:38 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 100 ms, Memory: 7928 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,40 +1,46 @@
 #include<iostream>
+#include<vector>
 #include<queue>
+#include<cstring>
 #define MAX 100001
 using namespace std;
 
-int N;
 vector<int> adj[MAX];
-int visited[MAX];
+int ans[MAX];
+int N;
 
-void bfs(){
+int bfs(){
     queue<int> q;
     q.push(1);
+    ans[1] = 1;
     
     while(!q.empty()){
         auto cur = q.front();
         q.pop();
         
-        for(auto idx : adj[cur]){
-            if(visited[idx] != 0) continue;
-            visited[idx] = cur;
-            q.push(idx);
+        for(auto n : adj[cur]){
+            if(ans[n] !=0) continue;
+            // 단위작업
+            q.push(n);
+            ans[n] = cur;
         }
     }
-    
+    return 0;
 }
 
-int main(void){
+
+int main(){
     int a,b;
     cin >> N;
-    
     for(int i=1; i<=N-1; i++){
         cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
     bfs();
+    
     for(int i=2; i<=N; i++){
-        cout << visited[i] << "\n";
+        cout << ans[i] << "\n";
     }
+    return 0;
 }
