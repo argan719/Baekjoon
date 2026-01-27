a967d9ead5dac36ab872e0fb7b63ae5d41b79dcb
argan719 <64789601+argan719@users.noreply.github.com>
2026-01-27 19:35:41 +0900

    [Gold V] Title: 토마토, Time: 228 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<vector>
 #include<cstring>
 #include<queue>
 #define MAX 101
@@ -16,9 +15,9 @@ typedef struct pos{
     int h;
 }pos;
 
-int dr[] = {-1, 1, 0, 0};
-int dc[] = {0, 0, -1, 1};
-int dh[] = {-1, 1};
+int dr[] = {-1, 1, 0, 0, 0, 0};
+int dc[] = {0, 0, -1, 1, 0, 0};
+int dh[] = {0, 0, 0, 0, -1, 1};
 
 void input(){
     cin >> M >> N >> H;
@@ -34,7 +33,6 @@ void input(){
 int bfs(){
     queue<pos> q;
     int nr, nc, nh;
-    pos tmp;
     int cnt = 0; // 익지 않은 토마토 개수
     
     // 단위작업 - 익은 토마토 전부 push
@@ -42,8 +40,7 @@ int bfs(){
         for(int i=0; i<N; i++){
             for(int j=0; j<M; j++){
                 if(matrix[i][j][k] == 1) {
-                    tmp.r = i; tmp.c = j; tmp.h = k;
-                    q.push(tmp);
+                    q.push({i, j, k});
                     v[i][j][k] = 1;
                 }
                 else if(matrix[i][j][k] == 0) cnt++;
@@ -53,41 +50,29 @@ int bfs(){
     
     if(cnt == 0) return 0; // 모든 토마토가 익어있는 상태
     
-    pos cur = q.front();
+    pos c;
     while(!q.empty()){
-        cur = q.front();
+        c = q.front();
         q.pop();
         
-        // 연결 4방향
-        for(int i=0; i<4; i++){
-            nr = cur.r + dr[i];
-            nc = cur.c + dc[i];
+        // 연결 6방향
+        for(int i=0; i<6; i++){
+            nr = c.r + dr[i];
+            nc = c.c + dc[i];
+            nh = c.h + dh[i];
             
             // 범위내 미방문 조건맞으면 (==0)
-            if(nr < 0 || nr >=N || nc < 0 || nc >=M) continue;
-            if(v[nr][nc][cur.h] != 0) continue;
-            if(matrix[nr][nc][cur.h] == 0){
+            if(nr < 0 || nr >=N || nc < 0 || nc >=M || nh < 0 || nh >= H) continue;
+            if(v[nr][nc][nh] != 0) continue;
+            if(matrix[nr][nc][nh] == 0){
                 // 단위작업
-                q.push({nr, nc, cur.h});
-                v[nr][nc][cur.h] = v[cur.r][cur.c][cur.h] + 1;
-                cnt--;
-            }
-        }
-        
-        // 연결 2방향
-        for(int i=0; i<2; i++){
-            nh = cur.h + dh[i];
-            // 범위내 미방문 조건맞으면 (==0)
-            if(nh < 0 || nh >= H) continue;
-            if(v[cur.r][cur.c][nh] !=0) continue;
-            if(matrix[cur.r][cur.c][nh] == 0){
-                q.push({cur.r, cur.c, nh});
-                v[cur.r][cur.c][nh] = v[cur.r][cur.c][cur.h] + 1;
+                q.push({nr, nc, nh});
+                v[nr][nc][nh] = v[c.r][c.c][c.h] + 1;
                 cnt--;
             }
         }
     }
-    if(cnt == 0) return v[cur.r][cur.c][cur.h] - 1;
+    if(cnt == 0) return v[c.r][c.c][c.h] - 1;
     else return -1;
     
 }
