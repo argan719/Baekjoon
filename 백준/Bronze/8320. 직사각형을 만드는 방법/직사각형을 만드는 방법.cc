75d04a8a2af4fdb299aca262e668da215c6aebcd
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-17 18:18:16 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 116 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.cc
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.cc
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
 
-int main(void){
-    int n;
-    cin >> n;
-    
+int N;
+int main(){
+    cin >> N;
     int cnt = 0;
-    cnt += n;
-    
-    for(int i=2; i<n; i++){
-        for(int j=i; j<n; j++){
-            if(i*j > n) break;
-            cnt++;
+    int sum = 0;
+    // 10^8
+    for(int i=1; i<=N; i++){
+        cnt = 0;
+        for(int j=1; j<=i; j++){
+            if(i % j == 0) cnt++;
         }
+        sum += cnt/2;
+        if(cnt % 2 != 0) sum++;
     }
-    cout << cnt;
-    
-    
+    cout << sum;
 }
