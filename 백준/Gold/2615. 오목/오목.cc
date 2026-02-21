bd31207cd24242f743650ad610a959c92d2ab0cd
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-21 13:32:33 +0900

    [Gold V] Title: 오목, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2615. 오목/오목.cc
+++ 백준/Gold/2615. 오목/오목.cc
@@ -1,10 +1,13 @@
 #include<iostream>
-#include<algorithm>
-#define MAX 20
 #define N 19
+#define MAX 21
 using namespace std;
 
 int matrix[MAX][MAX];
+
+// 우, 하, 우하(왼쪽대각선), 우상(오른쪽대각선)
+int dr[] = {0, 1, 1, -1};
+int dc[] = {1, 0, 1, 1};
 
 void input(){
     for(int i=1; i<=N; i++){
@@ -14,99 +17,42 @@ void input(){
     }
 }
 
-
 void solve(){
-    int r, c;
-    int cnt = 0;
-    int save_i, save_j;
-    
-    // 가로 방향
+    int val;
+    int cnt;
     for(int i=1; i<=N; i++){
         for(int j=1; j<=N; j++){
+            
             if(matrix[i][j] == 0) continue;
+            val = matrix[i][j];
             
-            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
-            if(matrix[i][j-1] == matrix[i][j]) continue;
-            
-            r = i, c = j, cnt = 1;
-            while(c <= N){
-                if(matrix[r][++c] == matrix[i][j]) cnt++;
-                else break;
-            }
-            if(cnt == 5) {
-                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
-                return;
+            for(int dir=0; dir<4; dir++){
+                int nr = i + dr[dir];
+                int nc = j + dc[dir];
+                
+                cnt = 1;
+                while(nr >= 1 && nr <= N && nc >= 1 && nc <= N && matrix[nr][nc] == val){
+                    cnt++;
+                    nr = nr + dr[dir];
+                    nc = nc + dc[dir];
+                }
+                if(cnt == 5){
+                    int pr = i - dr[dir];
+                    int pc = j - dc[dir];
+                    
+                    if(pr >= 1 && pr <= N && pc >=1 && pc <=N && val == matrix[pr][pc]) continue;
+                    cout << val << "\n" << i << " " << j << "\n";
+                    return;
+                }
             }
         }
     }
-    
-    // 세로 방향
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            if(matrix[i][j] == 0) continue;
-            
-            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
-            if(matrix[i-1][j] == matrix[i][j]) continue;
-            
-            r = i, c = j, cnt = 1;
-            while(r <= N){
-                if(matrix[++r][c] == matrix[i][j]) cnt++;
-                else break;
-            }
-            if(cnt == 5) {
-                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
-                return;
-            }
-        }
-    }
-    
-    // 오른쪽 대각선 방향
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<= N; j++){
-            if(matrix[i][j] == 0) continue;
-            
-            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
-            if(matrix[i-1][j-1] == matrix[i][j]) continue;
-            
-            r = i, c = j, cnt = 1;
-            while(c <= N){
-                if(matrix[++r][++c] == matrix[i][j]) cnt++;
-                else break;
-            }
-            if(cnt == 5) {
-                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
-                return;
-            }
-            
-        }
-    }
-    
-    // 왼쪽 대각선 방향 (맨 왼쪽이므로 다른 방향과 달리 맨 마지막 오목의 좌표를 출력해야함!)
-    for(int i=1; i<=N; i++){
-        for(int j=5; j<= N; j++){
-            if(matrix[i][j] == 0) continue;
-            
-            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
-            if(matrix[i-1][j+1] == matrix[i][j]) continue;
-            
-            r = i, c = j, cnt = 1;
-            while(c >= 1 && r <= N){
-                if(matrix[++r][--c] == matrix[i][j]) cnt++;
-                else break;
-            }
-            if(cnt == 5) {
-                cout << matrix[--r][++c] << "\n" << r << " " << c << "\n";
-                return;
-            }
-        
-        }
-    }
-    
-    // 무승부인 경우
-    cout << 0 << endl;
+    // 무승부인 경우!
+    cout << 0 << "\n";
 }
 
 int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL);
     input();
     solve();
 }
