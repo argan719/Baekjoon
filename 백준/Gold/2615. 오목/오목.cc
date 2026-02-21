bf17b1eb8569ea3aa4092f7db4dbf60af37889f8
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-21 13:05:19 +0900

    [Gold V] Title: 오목, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2615. 오목/오목.cc
+++ 백준/Gold/2615. 오목/오목.cc
@@ -14,6 +14,7 @@ void input(){
     }
 }
 
+
 void solve(){
     int r, c;
     int cnt = 0;
@@ -23,21 +24,19 @@ void solve(){
     for(int i=1; i<=N; i++){
         for(int j=1; j<=N; j++){
             if(matrix[i][j] == 0) continue;
-            // 중복 방지 조건 추가
+            
+            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
             if(matrix[i][j-1] == matrix[i][j]) continue;
-             
-                save_i = i, save_j = j;
-                r = i, c = j, cnt = 1;
-                while(c <= N){
-                    if(matrix[r][++c] == matrix[i][j]) cnt++;
-                    else break;
-                }
-                if(cnt == 5) {
-                    cout << matrix[save_i][save_j] << "\n" << save_i << " " << save_j << "\n";
-                    return;
-                }
             
-            
+            r = i, c = j, cnt = 1;
+            while(c <= N){
+                if(matrix[r][++c] == matrix[i][j]) cnt++;
+                else break;
+            }
+            if(cnt == 5) {
+                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
+                return;
+            }
         }
     }
     
@@ -46,23 +45,18 @@ void solve(){
         for(int j=1; j<=N; j++){
             if(matrix[i][j] == 0) continue;
             
+            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
             if(matrix[i-1][j] == matrix[i][j]) continue;
             
-            
-            
-                save_i = i, save_j = j;
-                r = i, c = j, cnt = 1;
-                while(r <= N){
-                    if(matrix[++r][c] == matrix[i][j]) cnt++;
-                    else break;
-                }
-                if(cnt == 5) {
-                    cout << matrix[save_i][save_j] << "\n" << save_i << " " << save_j << "\n";
-                    return;
-                }
-            
-            
-            
+            r = i, c = j, cnt = 1;
+            while(r <= N){
+                if(matrix[++r][c] == matrix[i][j]) cnt++;
+                else break;
+            }
+            if(cnt == 5) {
+                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
+                return;
+            }
         }
     }
     
@@ -71,47 +65,45 @@ void solve(){
         for(int j=1; j<= N; j++){
             if(matrix[i][j] == 0) continue;
             
+            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
             if(matrix[i-1][j-1] == matrix[i][j]) continue;
             
-                save_i = i, save_j = j;
-                r = i, c = j, cnt = 1;
-                while(c <= N){
-                    if(matrix[++r][++c] == matrix[i][j]) cnt++;
-                    else break;
-                }
-                if(cnt == 5) {
-                    cout << matrix[save_i][save_j] << "\n" << save_i << " " << save_j << "\n";
-                    return;
-                }
-            
+            r = i, c = j, cnt = 1;
+            while(c <= N){
+                if(matrix[++r][++c] == matrix[i][j]) cnt++;
+                else break;
+            }
+            if(cnt == 5) {
+                cout << matrix[i][j] << "\n" << i << " " << j << "\n";
+                return;
+            }
             
         }
     }
     
-    // 왼쪽 대각선 방향
+    // 왼쪽 대각선 방향 (맨 왼쪽이므로 다른 방향과 달리 맨 마지막 오목의 좌표를 출력해야함!)
     for(int i=1; i<=N; i++){
         for(int j=5; j<= N; j++){
             if(matrix[i][j] == 0) continue;
             
+            // 중복 방지 조건 추가 (첫번째 오목이 아님을 감지 - 6개 이상 막기 위함)
             if(matrix[i-1][j+1] == matrix[i][j]) continue;
             
-                save_i = i, save_j = j;
-                r = i, c = j, cnt = 1;
-                while(c >= 1 && r <= N){
-                    if(matrix[++r][--c] == matrix[i][j]) { cnt++; save_i = r; save_j = c;}
-                    else break;
-                }
-                if(cnt == 5) {
-                    cout << matrix[save_i][save_j] << "\n" << save_i << " " << save_j << "\n";
-                    return;
-                }
-            
-            
+            r = i, c = j, cnt = 1;
+            while(c >= 1 && r <= N){
+                if(matrix[++r][--c] == matrix[i][j]) cnt++;
+                else break;
+            }
+            if(cnt == 5) {
+                cout << matrix[--r][++c] << "\n" << r << " " << c << "\n";
+                return;
+            }
+        
         }
     }
     
+    // 무승부인 경우
     cout << 0 << endl;
-    
 }
 
 int main(){
