8f661c4bcbed4ca56464a04de33c58048b337e0b
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-21 15:52:02 +0900

    [Bronze II] Title: 시험 감독, Time: 100 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.cc
+++ 백준/Bronze/13458. 시험 감독/시험 감독.cc
@@ -20,19 +20,14 @@ void input(){
 
 void solve(){
     ll sum = 0;
-    ll cnt = 0;
     
     for(auto n : arr){
-        cnt = 0;
+        sum++;
+        if(n <= B) continue;
         
-        n -= B; cnt++;
-        // 여기서 n이 0뿐만 아니라 음수가 된 경우 고려 필요.
-        if(n > 0) {  // n이 1이상인 경우에만 추가 작업 필요.
-            cnt += n / C;
-            if(n % C !=0) cnt++;
-        }
-        
-        sum += cnt;
+        n -= B;
+        sum += n / C;
+        if(n % C !=0) sum++;
     }
     cout << sum << "\n";
 }
