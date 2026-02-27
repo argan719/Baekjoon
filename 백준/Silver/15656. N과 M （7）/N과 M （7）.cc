3e17a3ed095fd2a47ac85e895693f56b88b82049
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-27 09:31:35 +0900

    [Silver III] Title: N과 M (7), Time: 396 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#define MAX 10
+using namespace std;
+
+int N, M;
+int arr[MAX];
+int ans[MAX];
+
+void input(){
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+N);
+}
+
+void dfs(int n){
+    // 종료조건은 이곳에서
+    if(n == M){
+        for(int j=0; j<M; j++) cout << ans[j] << " ";
+        cout << "\n";
+        return;
+    }
+    
+    // 하부 함수 호출
+    for(int j=0; j<N; j++){
+        ans[n] = arr[j];
+        dfs(n+1);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    input();
+    dfs(0);
+}
