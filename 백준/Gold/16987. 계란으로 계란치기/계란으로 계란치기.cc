cb28b5a3d38462d9925964414d9d01b22f831203
argan719 <64789601+argan719@users.noreply.github.com>
2026-02-28 14:53:39 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 52 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<cstring>
-#include<algorithm>
 #define MAX 10
 using namespace std;
 
@@ -30,27 +29,28 @@ void restore(int n, int target){
     if(arr[target].s > 0) crack[target] = 0;
 }
 
-void dfs(int n){
+void dfs(int n, int cnt){
     // 종료조건은 이곳에서
     if(n == N){ // 한번씩 계란을 다 들어봤다면 종료.
-        int cnt = 0;
-        for(int i=0; i<N; i++){
-            if(crack[i]) cnt++;
-        }
-        ans = max(ans, cnt);
+        if(ans < cnt) ans = cnt;
         return;
     }
     
     // 하부함수 호출 (중복가능)
-    if(crack[n] == 1) dfs(n+1);
+    if(crack[n] == 1) dfs(n+1, cnt);
     else{
         int flag = 0;
         for(int j=0; j<N; j++){
             if(n == j) continue;
             
-            if(crack[j] == 0) { flag = 1; check(n, j);  dfs(n+1);  restore(n, j);}
+            if(crack[j] == 0) {
+                flag = 1;
+                check(n, j);
+                dfs(n+1, cnt+crack[n]+crack[j]);
+                restore(n, j);
+            }
         }
-        if(flag == 0) dfs(n+1);
+        if(flag == 0) dfs(n+1, cnt);
     }
     
 }
@@ -58,6 +58,6 @@ void dfs(int n){
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
-    dfs(0);
+    dfs(0, 0);
     cout << ans;
 }
