c828973c3abc86b0a4e581c1dc52a180cdfd1c48
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-04 16:08:37 +0900

    [Platinum V] Title: 비숍, Time: 132 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/1799. 비숍/비숍.cc
+++ 백준/Platinum/1799. 비숍/비숍.cc
@@ -6,20 +6,22 @@ using namespace std;
 int N;
 int matrix[MAX][MAX];
 int ans;
-vector<int> bishop;
 int vLeft[2*MAX];
+
+vector<pair<int,int>> bishop[2*MAX];
 
 void input(){
     cin >> N;
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             cin >> matrix[i][j];
-            if(matrix[i][j] == 1) bishop.push_back(i*N+j);
+            if(matrix[i][j] == 1) bishop[i+j].push_back({i, j});
         }
     }
 }
 
 void dfs(int n, int cnt){
+    if(ans >= (2*N - 1) - n + cnt) return;
     // 종료조건
     if(n == 2*N -1){
         if(ans < cnt) ans = cnt;
@@ -27,12 +29,11 @@ void dfs(int n, int cnt){
     }
     
     int flag = 0;
-    for(int cur : bishop){
-        int r = cur/N, c = cur%N;
-        if(r + c == n && vLeft[r-c+N] == 0) {
-            vLeft[r-c+N] = 1;
+    for(auto cur : bishop[n]){
+        if(vLeft[cur.first - cur.second + N] == 0) {
+            vLeft[cur.first - cur.second + N] = 1;
             dfs(n+1, cnt+1);
-            vLeft[r-c+N] = 0;
+            vLeft[cur.first - cur.second + N] = 0;
             flag = 1;
         }
     }
