08c57f559affc9b840a494ed78b297feb13cd8fb
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-05 11:32:46 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -1,49 +1,51 @@
 #include<iostream>
-#include<cstdlib>
-#include<queue>
-#include<climits>
 #define MAX 11
 using namespace std;
-typedef long long ll;
 
 int N;
-ll arr[MAX][2];
-int min_val = INT_MAX;
-int visited[MAX];
-vector<int> vec;
+//int arr[MAX][2]; // 신맛, 쓴맛,
+struct taste{
+    int s, b;
+};
+taste arr[MAX];
+int v[MAX];  // 해당 재료 포함시킴 표시
+int ans = 1100000000;
 
-//n : 담은 재료 갯수
+void input(){
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> arr[i].s >> arr[i].b;
+    }
+}
 
-void dfs(int n, int cur, ll sum_a, ll sum_b){
-    int sum = abs(sum_a - sum_b);
-    if(min_val > sum && n > 0) min_val = sum;
-    
+void dfs(int n, int cnt){
+    // 종료조건
     if(n == N){
+        // 정답처리
+        if(cnt == 0) return;  // 재료가 하나도 선택되지 않은 경우는 제외
+        
+        int sum_s = 1, sum_b = 0;
+        for(int i=0; i<N; i++){
+            if(v[i]) {
+                sum_s *= arr[i].s;
+                sum_b += arr[i].b;
+            }
+        }
+        if(ans > abs(sum_s - sum_b)) ans = abs(sum_s - sum_b);
         return;
     }
     
-    for(int i=1; i<=N; i++){
-        if(visited[i]) continue;
-        visited[i] = 1;
-        dfs(n+1, i, sum_a*arr[i][0], sum_b+arr[i][1]);
-        visited[i] = 0;
-        sum_a = 1;
-        sum_b = 0;
-        n = 0;
-    }
-
+    // 해당 재료를 포함하는 경우
+    v[n] = 1;
+    dfs(n+1, cnt+1);
+    v[n] = 0;
+    // 해당 재료를 미포함하는 경우
+    dfs(n+1, cnt);
 }
 
-
-
-
-int main(void){
-    cin >> N;
-    for(int i=1; i<=N; i++){
-        cin >> arr[i][0] >> arr[i][1];  // 신맛 쓴맛
-    }
-    
-    dfs(0, 0, 1, 0);
-    cout << min_val;
-    
+int main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    input();
+    dfs(0, 0);
+    cout << ans;
 }
