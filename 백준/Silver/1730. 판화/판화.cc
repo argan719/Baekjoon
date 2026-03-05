8daf86cd20f77cb1a81c97527bdca2d22312815d
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-05 13:11:46 +0900

    [Silver IV] Title: 판화, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1730. 판화/판화.cc
+++ 백준/Silver/1730. 판화/판화.cc
@@ -2,40 +2,40 @@
 #include<cstring>
 #define MAX 11
 using namespace std;
-
 int N;
-string cmd;
 int matrix[MAX][MAX];
+string cmd;
 
 void input(){
-    cin >> N >> cmd;
+    cin >> N;
+    cin >> cmd;
 }
 
 void solve(){
     int r = 0, c = 0;
+    
     for(auto n : cmd){
+        
         if(n == 'U' && r - 1 >= 0){
-            matrix[r--][c] |= 2;
-            matrix[r][c] |= 2;
+            matrix[r--][c] |= 1;
+            matrix[r][c] |= 1;
         }
         else if(n == 'D' && r + 1 < N){
-            matrix[r++][c] |= 2;
-            matrix[r][c] |= 2;
+            matrix[r++][c] |= 1;
+            matrix[r][c] |= 1;
         }
         else if(n == 'L' && c - 1 >= 0){
-            matrix[r][c--] |= 1;
-            matrix[r][c] |= 1;
+            matrix[r][c--] |= 2;
+            matrix[r][c] |= 2;
         }
         else if(n == 'R' && c + 1 < N){
-            matrix[r][c++] |= 1;
-            matrix[r][c] |= 1;
+            matrix[r][c++] |= 2;
+            matrix[r][c] |= 2;
         }
-        
     }
-}
-
-string ans = ".-|+";
-void print(){
+    
+    // 정답출력
+    string ans = ".|-+";
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             cout << ans[matrix[i][j]];
@@ -45,8 +45,7 @@ void print(){
 }
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(0);
     input();
     solve();
-    print();
 }
