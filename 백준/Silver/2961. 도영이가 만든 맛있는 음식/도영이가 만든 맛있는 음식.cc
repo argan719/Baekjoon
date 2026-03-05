acbe228e0b1134ef70ee10d9b633de28e2bd9dee
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-05 11:38:45 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.cc
@@ -3,13 +3,12 @@
 using namespace std;
 
 int N;
-//int arr[MAX][2]; // 신맛, 쓴맛,
 struct taste{
     int s, b;
 };
 taste arr[MAX];
-int v[MAX];  // 해당 재료 포함시킴 표시
-int ans = 1100000000;
+//int v[MAX];  // 해당 재료 포함시킴 표시
+int ans = (int)1e9;
 
 void input(){
     cin >> N;
@@ -18,34 +17,25 @@ void input(){
     }
 }
 
-void dfs(int n, int cnt){
+void dfs(int n, int mul, int sum, int cnt){
     // 종료조건
     if(n == N){
         // 정답처리
         if(cnt == 0) return;  // 재료가 하나도 선택되지 않은 경우는 제외
         
-        int sum_s = 1, sum_b = 0;
-        for(int i=0; i<N; i++){
-            if(v[i]) {
-                sum_s *= arr[i].s;
-                sum_b += arr[i].b;
-            }
-        }
-        if(ans > abs(sum_s - sum_b)) ans = abs(sum_s - sum_b);
+        if(ans > abs(mul - sum)) ans = abs(mul - sum);
         return;
     }
     
     // 해당 재료를 포함하는 경우
-    v[n] = 1;
-    dfs(n+1, cnt+1);
-    v[n] = 0;
+    dfs(n+1, mul * arr[n].s, sum + arr[n].b, cnt+1);
     // 해당 재료를 미포함하는 경우
-    dfs(n+1, cnt);
+    dfs(n+1, mul, sum, cnt);
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
-    dfs(0, 0);
+    dfs(0, 1, 0, 0);  // n, mul, sum, cnt
     cout << ans;
 }
