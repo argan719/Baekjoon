a99782736eae2dbbea56ed1ea516f795b5c7055a
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-06 12:29:05 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -2,64 +2,38 @@
 #define MAX 12
 using namespace std;
 
-int arr[4];  // +, -, x, / 연산자 갯수 저장
-int num[MAX];
-int N, M;
-int ans[MAX];
+int arr[MAX];
+// +, -, x, / 연산자 갯수 저장
+int add, sub, mul, dv;
+int N;
 
-int max_ans = -1e9;
-int min_ans = 1e9;
+int ans_max = -1e9;
+int ans_min = 1e9;
 
 void input(){
     cin >> N;
-    M = N - 1;
-    for(int i=0; i<N; i++) cin >> num[i];
-    for(int i=0; i<4; i++) cin >> arr[i];
+    for(int i=0; i<N; i++) cin >> arr[i];
+    cin >> add >> sub >> mul >> dv;
 }
 
-void dfs(int n){
-    if(n == M){
+void dfs(int n, int sum, int add, int sub, int mul, int dv){
+    if(n == N){
         // 정답처리
-        int sum = 0;
-        int tmp[MAX] = {0, };
-        for(int i=0; i<N; i++) tmp[i] = num[i];
-        
-        for(int i=0; i<N-1; i++){
-            if(ans[i] == 0){
-                sum = tmp[i] + tmp[i+1];
-            }
-            else if(ans[i] == 1){
-                sum = tmp[i] - tmp[i+1];
-            }
-            else if(ans[i] == 2){
-                sum = tmp[i] * tmp[i+1];
-            }
-            else if(ans[i] == 3){
-                if(tmp[i] < 0) {
-                    sum = (abs(tmp[i])/tmp[i+1]) * -1;
-                }
-                else sum = tmp[i]/tmp[i+1];
-            }
-            tmp[i+1] = sum;
-        }
-        if(sum > max_ans) max_ans = sum;
-        if(sum < min_ans) min_ans = sum;
+        if(sum > ans_max) ans_max = sum;
+        if(sum < ans_min) ans_min = sum;
         
         return;
     }
     
-    for(int j=0; j<4; j++){
-        if(arr[j] <= 0) continue;
-        arr[j]--;
-        ans[n] = j;
-        dfs(n+1);
-        arr[j]++;
-    }
+    if(add > 0) dfs(n+1, sum + arr[n], add-1, sub, mul, dv);
+    if(sub > 0) dfs(n+1, sum - arr[n], add, sub-1, mul, dv);
+    if(mul > 0) dfs(n+1, sum * arr[n], add, sub, mul-1, dv);
+    if(dv > 0) dfs(n+1, sum/arr[n], add, sub, mul, dv-1);
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
-    dfs(0);
-    cout << max_ans << "\n" << min_ans << "\n";
+    dfs(1, arr[0], add, sub, mul, dv);  // n, sum, add, sub, mul, div
+    cout << ans_max << "\n" << ans_min << "\n";
 }
