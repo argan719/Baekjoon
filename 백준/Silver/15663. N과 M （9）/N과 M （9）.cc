8423c87c1b598684cdb010a7ed0e91e8766ebc0d
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-09 16:51:32 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,34 +1,32 @@
 #include<iostream>
 #include<algorithm>
-#define MAX 10
+#define MAX 10001
 using namespace std;
 
-int ans[MAX];
-int arr[MAX];
 int N, M;
+int arr[MAX];
 int v[MAX];
+int ans[MAX];
 
 void input(){
     cin >> N >> M;
-    for(int i=0; i<N; i++) {
-        cin >> arr[i];
-    }
+    for(int i=0; i<N; i++) cin >> arr[i];
     sort(arr, arr+N);
 }
 
 void dfs(int n){
-    // 종료조건은 여기에서
+    // 종료조건
     if(n == M){
-        for(int j=0; j<M; j++) cout << ans[j] << " ";
+        for(int i=0; i<M; i++) cout << ans[i] << " ";
         cout << "\n";
         return;
     }
     
-    // 하부함수 호출
-    int prev = 0;
+    int prev = -1;
     for(int j=0; j<N; j++){
         if(v[j]) continue;
         if(prev == arr[j]) continue;
+        
         v[j] = 1;
         prev = arr[j];
         ans[n] = arr[j];
@@ -41,5 +39,4 @@ int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
     dfs(0);
-    
 }
