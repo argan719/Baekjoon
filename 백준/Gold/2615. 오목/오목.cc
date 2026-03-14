af5d66edf42147fcdbcf524fa09dca4307182f1d
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-14 12:40:22 +0900

    [Gold V] Title: 오목, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2615. 오목/오목.cc
+++ 백준/Gold/2615. 오목/오목.cc
@@ -1,58 +1,63 @@
 #include<iostream>
-#define N 19
-#define MAX 21
+#define MAX 20
 using namespace std;
 
 int matrix[MAX][MAX];
+int ans;
+int ans_i, ans_j;
+int N = 19;
 
-// 우, 하, 우하(왼쪽대각선), 우상(오른쪽대각선)
-int dr[] = {0, 1, 1, -1};
-int dc[] = {1, 0, 1, 1};
+// 우상 우 우하 하
+int dr[] = {-1, 0, 1, 1};
+int dc[] = {1, 1, 1, 0};
 
 void input(){
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
             cin >> matrix[i][j];
         }
     }
 }
 
+void check(int r, int c, int dir){
+    int nr = r; int nc = c;
+    int val = matrix[r][c];
+    int cnt = 1; // 본인 포함.
+    
+    while(1){
+        nr += dr[dir]; nc += dc[dir];
+        if(nr < 0 || nr >= N || nc < 0 || nc >= N) break;
+        if(matrix[nr][nc] != val) break;
+        
+        cnt++;
+    }
+    if(cnt == 5) {
+        cout << val << "\n";
+        cout << r+1 << " " << c+1 << "\n";
+        exit(0);
+    }
+}
+
 void solve(){
-    int val;
-    int cnt;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
             if(matrix[i][j] == 0) continue;
-            val = matrix[i][j];
             
             for(int dir=0; dir<4; dir++){
-                int nr = i + dr[dir];
-                int nc = j + dc[dir];
+                int nr =  i - dr[dir]; int nc = j - dc[dir];
+                if(matrix[nr][nc] == matrix[i][j]) continue;
                 
-                cnt = 1;
-                while(nr >= 1 && nr <= N && nc >= 1 && nc <= N && matrix[nr][nc] == val){
-                    cnt++;
-                    nr = nr + dr[dir];
-                    nc = nc + dc[dir];
-                }
-                if(cnt == 5){
-                    int pr = i - dr[dir];
-                    int pc = j - dc[dir];
-                    
-                    if(pr >= 1 && pr <= N && pc >=1 && pc <=N && val == matrix[pr][pc]) continue;
-                    cout << val << "\n" << i << " " << j << "\n";
-                    return;
-                }
+                check(i, j, dir);
             }
+            
         }
     }
-    // 무승부인 경우!
+    // 승부가 나지 않은 경우
     cout << 0 << "\n";
 }
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(0);
     input();
     solve();
 }
