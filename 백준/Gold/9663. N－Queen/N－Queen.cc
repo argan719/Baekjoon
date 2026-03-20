5de0488a3ef9f6d031ce14343346f3a1c6a853ac
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-20 14:12:33 +0900

    [Gold IV] Title: N-Queen, Time: 1648 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,10 +1,8 @@
 #include<iostream>
-#include<cstring>
-#define MAX 15
+#define MAX 16
 using namespace std;
 int N;
-int matrix[MAX][MAX];
-int vUp[MAX];
+int vRow[MAX];
 int vLeft[2*MAX];
 int vRight[2*MAX];
 int ans;
@@ -19,18 +17,18 @@ void dfs(int n){
         return;
     }
     
-    // 하부함수 호출
     for(int j=0; j<N; j++){
-        // (열 선택)조건
-        if(vUp[j] == 1 || vLeft[n-j+N] == 1 || vRight[n+j] == 1 ) continue;
-        
-        // 표시
-        vUp[j] = 1; vLeft[n-j+N] = 1; vRight[n+j] = 1;
+        if(vRow[j] || vRight[n+j] || vLeft[n-j + N]) continue;
+        // 방문처리
+        vRow[j] = 1;
+        vRight[n + j] = 1;
+        vLeft[n - j + N] = 1;
         
         dfs(n+1);
         
-        // 원상복구
-        vUp[j] = 0; vLeft[n-j+N] = 0; vRight[n+j] = 0;
+        vRow[j] = 0;
+        vRight[n + j] = 0;
+        vLeft[n - j + N] = 0;
     }
 }
 
@@ -38,5 +36,5 @@ int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
     dfs(0);
-    cout << ans;
+    cout << ans << "\n";
 }
