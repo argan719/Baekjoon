9e2b780e63df3bd10de4e70a0d2778c1d5769b92
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-27 09:38:56 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -6,8 +6,9 @@
 using namespace std;
 
 vector<pair<int,int>> pos;
+vector<pair<int,int>> virus;
 int matrix[MAX][MAX];
-int matrix2[MAX][MAX];
+//int matrix2[MAX][MAX];
 int v[MAX][MAX];
 
 int dr[] = {-1, 1, 0, 0};
@@ -22,9 +23,9 @@ void input(){
         for(int j=0; j<M; j++){
             cin >> r;
             matrix[i][j] = r;
-            if(r == 0) {
-                pos.push_back({i, j});
-            }
+            
+            if(r == 0) pos.push_back({i, j}); // 빈칸 리스트
+            else if(r == 2) virus.push_back({i, j}); // 바이러스 리스트
         }
     }
 }
@@ -35,15 +36,23 @@ int bfs(){
     int nr, nc;
     memset(v, 0, sizeof(v));
     
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(matrix2[i][j] == 2) {
-                // 단위작업 - 바이러스 전부 push
-                q.push(make_pair(i,j));
-                v[i][j] = 1;
-            }
-        }
+//    for(int i=0; i<N; i++){
+//        for(int j=0; j<M; j++){
+//            if(matrix2[i][j] == 2) {
+//                // 단위작업 - 바이러스 전부 push
+//                q.push(make_pair(i,j));
+//                v[i][j] = 1;
+//            }
+//        }
+//    }
+    // 단위작업 - 바이러스 전부 push
+    for(auto cur : virus){
+        q.push(cur);
+        v[cur.first][cur.second] = 1;
     }
+    
+    
+    int cnt = pos.size() - 3;  // 현재 (벽을 3개 세우고 난뒤) 빈칸 수
     
     while(!q.empty()){
         auto cur = q.front();
@@ -57,22 +66,25 @@ int bfs(){
             if(nr < 0 || nr >= N || nc < 0 || nc >=M) continue;
             if(v[nr][nc] != 0) continue;
             
-            if(matrix2[nr][nc] == 0){
-                matrix2[nr][nc] = 2;
+            if(matrix[nr][nc] == 0){
+                //matrix[nr][nc] = 2;
                 q.push(make_pair(nr, nc));
                 v[nr][nc] = 1;
+                cnt--; // 감염되어 빈칸 사라짐.
             }
         }
     }
     
-    int cnt = 0;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<M; j++){
-            if(matrix2[i][j] == 0) {
-                cnt++;
-            }
-        }
-    }
+    // 벽을 3개 세운 뒤, 바이러스 퍼진 뒤, 남아있는 빈칸의 개수
+//    int cnt = 0;
+//    for(int i=0; i<N; i++){
+//        for(int j=0; j<M; j++){
+//            if(matrix[i][j] == 0) {
+//                cnt++;
+//            }
+//        }
+//    }
+    // 이중 루프 돌 필요 없이
     return cnt;
     
 }
@@ -82,25 +94,27 @@ int main(){
     
     int max = 0;
     int result;
+    
     for(int i=0; i< pos.size()-2; i++){
-        if(matrix[pos[i].first][pos[i].second] != 0) continue;
-        for(int j=1+i; j< pos.size()-1; j++){
-            if(matrix[pos[j].first][pos[j].second] != 0) continue;
-            for(int k=1+j; k< pos.size(); k++){
-                if(matrix[pos[k].first][pos[k].second] != 0) continue;
+        for(int j=i+1; j< pos.size()-1; j++){
+            for(int k=j+1; k< pos.size(); k++){
                 
-                memcpy(matrix2, matrix, sizeof(matrix));
+                //memcpy(matrix2, matrix, sizeof(matrix));
                 
-                matrix2[pos[i].first][pos[i].second] = 1;
-                matrix2[pos[j].first][pos[j].second] = 1;
-                matrix2[pos[k].first][pos[k].second] = 1;
+                matrix[pos[i].first][pos[i].second] = 1; // 벽 세움1
+                matrix[pos[j].first][pos[j].second] = 1; // 벽 세움2
+                matrix[pos[k].first][pos[k].second] = 1; // 벽 세움3
                 
                 result = bfs();
                 if(max < result) max = result;
+                
+                matrix[pos[k].first][pos[k].second] = 0; // 벽 세움3 원상복수
             }
+            matrix[pos[j].first][pos[j].second] = 0; // 벽 세움2 원상복구
         }
+        matrix[pos[i].first][pos[i].second] = 0; // 벽 세움1 원상복구
     }
-    cout << max;
+    cout << max << endl;
     
     return 0;
 }
