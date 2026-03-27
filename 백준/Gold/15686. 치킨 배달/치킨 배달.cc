30063aeace33c8d9707192025c62343c19661cc5
argan719 <64789601+argan719@users.noreply.github.com>
2026-03-27 13:17:00 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,61 +1,62 @@
 #include<iostream>
-#include<algorithm>
 #include<vector>
+#include<cstring>
+#include<algorithm>
 #define MAX 51
 using namespace std;
 
 int N, M;
 int matrix[MAX][MAX];
-int v[15];
-int s[15];
-int ans = 100 * 2 * 50; // 최솟값
+int v[MAX];
 
-vector<pair<int, int>> chicken;
-vector<pair<int, int>> house;
+struct Pos{
+    int r, c;
+};
+vector<Pos> house;
+vector<Pos> chicken;
 
-int CCNT;
+int mlst[15]; // 선정한 치킨집. 0~M-1
+int ans = 1e5;
 
 void input(){
     cin >> N >> M;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
             cin >> matrix[i][j];
-            if(matrix[i][j] == 1) house.push_back({i,j});
-            else if(matrix[i][j] == 2) chicken.push_back({i,j});
+            if(matrix[i][j] == 0) continue;
+            if(matrix[i][j] == 1) house.push_back({i,j});  // 집 좌표
+            else if(matrix[i][j] == 2) chicken.push_back({i,j}); // 치킨집 좌표
         }
     }
-    CCNT = chicken.size();
-}
-
-void check(){
-    int dist = 0;
-    int min_dist = 100;
-    int sum = 0;
-    for(auto h : house){
-        min_dist = 100;
-        for(int i=0; i<M; i++){
-            dist = abs(h.first - chicken[s[i]].first) + abs(h.second - chicken[s[i]].second);
-            min_dist = min(min_dist, dist);
-        }
-        sum += min_dist;
-    }
-    ans = min(ans, sum);
 }
 
 void dfs(int n, int start){
+    // 종료조건은 이곳에서
     if(n == M){
-        // 정답처리
-        check();
+        // 치킨거리의 합 구해서 최솟값 갱신.
+        int sum = 0;
+        for(auto h : house){
+            int house_min = 1000;
+            for(int i=0; i<M; i++){
+                house_min = min(house_min, abs(chicken[mlst[i]].r - h.r) + abs(chicken[mlst[i]].c - h.c));
+            }
+            sum += house_min;
+        }
+        ans = min(ans, sum);
         return;
     }
     
-    for(int j=start; j<CCNT; j++){
-        s[n] = j;  // 선택한 치킨집 인덱스
-        dfs(n+1, j+1);
+    for(int i=start; i<chicken.size(); i++){
+        //if(v[i]) continue;
+        //v[i] = 1;
+        mlst[n] = i;
+        dfs(n+1, i+1);
+        //v[i] = 0;
     }
 }
 
-int main(){
+
+int main(void){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
     dfs(0, 0);
