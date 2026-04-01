624799ffe2406554bd1f04cc46ecab947806af10
argan719 <64789601+argan719@users.noreply.github.com>
2026-04-01 13:51:21 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 32 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -29,9 +29,8 @@ void input(){
             if(matrix[i][j] == -1){
                 if(flag == 0) {
                     flag = 1;
-                    first = i;
+                    first = i; second = i+1;
                 }
-                else second = i;
                 matrix[i][j] = 0;  // 0으로 변환.
             }
         }
@@ -41,30 +40,33 @@ void input(){
 void solve(){
     for(int t=1; t<=T; t++){
         // [1] 미세먼지 확산
-        memset(tmp, 0, sizeof(tmp));
+        //memset(tmp, 0, sizeof(tmp));
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=M; j++)
+                tmp[i][j] = matrix[i][j];
+        
         
         for(int r=1; r<=N; r++){
             for(int c=1; c<=M; c++){
-                // 미세먼지가 있다면 확산.
-                if(matrix[r][c] > 0) {
-                    int cnt = 0;
+                // 미세먼지가 4보다 클 때만 확산.
+                if(matrix[r][c] > 4) {
                     for(int dir=0; dir<4; dir++){
                         int nr =  r + dr[dir]; int nc = c + dc[dir];
                         if(matrix[nr][nc] == -1) continue;
                         if(nc == 1 && (nr == first || nr == second)) continue;
                         tmp[nr][nc] += matrix[r][c]/5;
-                        cnt++;
+                        tmp[r][c] -= matrix[r][c]/5;
                     }
-                    tmp[r][c] += matrix[r][c] - (matrix[r][c]/5) * cnt;
+                    //tmp[r][c] += matrix[r][c] - (matrix[r][c]/5) * cnt;
                 }
             }
         }
         
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M;j++){
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=M;j++)
                 matrix[i][j] = tmp[i][j];
-            }
-        }
+            
+        
  
         // [2] 공기청정기 바람 순환
         // 아랫 바람부터 - 시계 방향
@@ -74,17 +76,12 @@ void solve(){
         while(1){
             ni = ci + dr[d]; nj = cj + dc[d];
             // 공기청정기 좌표로 돌아왔다면 - 종료
-            if(ni == second && nj == 1) {
-                // 공기청정기 처리
-                //tmp[ni][nj] = -1;
-                break;
-            }
-            
+            if(ni == second && nj == 1) break;
+          
             if(matrix[ni][nj] == -1) {
                 d = (d+1)%4;
                 ni = ci + dr[d]; nj = cj + dc[d];
             }
-            //if(matrix[ci][cj] == -1) tmp[ni][nj] = 0;
             tmp[ni][nj] = matrix[ci][cj];
             ci = ni; cj = nj;
         }
@@ -94,26 +91,21 @@ void solve(){
         while(1){
             ni = ci + dr[d]; nj = cj + dc[d];
             // 공기청정기 좌표로 돌아왔다면 - 종료
-            if(ni == first && nj == 1) {
-                // 공기청정기 처리
-                //tmp[ni][nj] = -1;
-                break;
-            }
+            if(ni == first && nj == 1) break;
+           
             // 범위 밖이라면 - 방향 전환
             if(matrix[ni][nj] == -1) {
                 d = (d-1+4)%4;
                 ni = ci + dr[d]; nj = cj + dc[d];
             }
-            //if(matrix[ci][cj] == -1) tmp[ni][nj] = 0;
             tmp[ni][nj] = matrix[ci][cj];
             ci = ni; cj = nj;
         }
         
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=M;j++){
+        for(int i=1; i<=N; i++)
+            for(int j=1; j<=M;j++)
                 matrix[i][j] = tmp[i][j];
-            }
-        }
+            
         
     }
     
