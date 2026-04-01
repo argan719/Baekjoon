c062c8d4802319c4364528ed5a8f1eedc3088b39
argan719 <64789601+argan719@users.noreply.github.com>
2026-04-01 11:14:24 +0900

    [Gold I] Title: 상어 중학교, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/21609. 상어 중학교/상어 중학교.cc
+++ 백준/Gold/21609. 상어 중학교/상어 중학교.cc
@@ -17,24 +17,11 @@ int tmp[MAX][MAX];
 int dr[] = {-1, 1, 0, 0};
 int dc[] = {0, 0, -1, 1};
 
-int cnt;
-int rcnt;
-
-struct Group{
-    int i, j;
-    int cnt;
-    int rcnt;
-    //int my_r; int my_c; // 해당 그룹의 기준 블록
+struct pos{
+    int i,j;
 };
-vector<Group> group;
-
-bool comp(Group a, Group b){
-    if(a.cnt != b.cnt) return a.cnt > b.cnt;
-    if(a.rcnt != b.rcnt) return a.rcnt > b.rcnt;
-    if(a.i != b.i) return a.i > b.i;
-    return a.j > b.j;
-    //return a.second > b.second;
-}
+vector<pair<int,int>> group;
+vector<pair<int,int>> del_group;
 
 void input(){
     cin >> N >> M;
@@ -49,174 +36,92 @@ void input(){
     }
 }
 
-void bfs(int r, int c){
-    //memset(v, 0, sizeof(v));
-    queue<pair<int,int>> q;
-    q.push({r,c});
-    v[r][c] = 1;
-    int val = matrix[r][c];
-    //cnt++;
-    
-    while(!q.empty()){
-        auto cur = q.front();
-        q.pop();
-        
-        // 정답처리
-        cnt++;  // 총 블록의 수 카운트.
-        if(matrix[cur.first][cur.second] == R) rcnt++;  // 무지개 블록 수 따로 카운트.
-        
-        // 상하좌우
-        for(int dir=0; dir<4; dir++){
-            int nr = cur.first + dr[dir]; int nc = cur.second + dc[dir];
-            if(nr < 1 || nr > N || nc < 1 || nc > N) continue;
-            if(matrix[nr][nc] == -1) continue;  // 범위내
-            if(v[nr][nc] == 1) continue;  // 미방문
+void gravity(){
+    int ci, cj;
+    for(int si=1; si<N; si++){
+        for(int sj=1; sj<=N; sj++){
+            ci = si; cj = sj;
             
-            // 무지개거나 나랑 값이 같은 일반블록이거나
-            if(matrix[nr][nc] == R || matrix[nr][nc] == val){
-                q.push({nr,nc});
-                v[nr][nc] = 1;
+            while(0 < matrix[ci][cj] && matrix[ci+1][cj] == 0){
+                int tmp = matrix[ci][cj];
+                matrix[ci][cj] = matrix[ci+1][cj];
+                matrix[ci+1][cj] = tmp;
+                ci--;
             }
         }
     }
 }
 
 
-void bfs_clear(int r, int c, int flag){
-    queue<pair<int,int>> q;
-    q.push({r,c});
-    v[r][c] = 1;
-    int val = matrix[r][c];
-    //cnt++;
+vector<pair<int,int>> bfs(){
+    vector<pair<int,int>> group_max;
+    memset(v, 0, sizeof(v));
+    pair<int,int> tmp;
+    int rcnt_max = 0;
     
-    while(!q.empty()){
-        auto cur = q.front();
-        q.pop();
-        
-        // 정답처리 - clear
-        if(flag == 1) matrix[cur.first][cur.second] = 0;
-        
-        
-        // 상하좌우
-        for(int dir=0; dir<4; dir++){
-            int nr = cur.first + dr[dir]; int nc = cur.second + dc[dir];
-            if(nr < 1 || nr > N || nc < 1 || nc > N) continue;
-            if(matrix[nr][nc] == -1) continue;  // 범위내
-            if(v[nr][nc] == 1) continue;  // 미방문
+    for(int i=1; i<=N; i++){
+        for(int j=1;j<=N; j++){
+            if(v[i][j] == 1) continue;
+            if(matrix[i][j] < 1 || matrix[i][j] == R) continue;
+            // 미방문, 일반 블록인 경우 bfs.
             
-            // 무지개거나 나랑 값이 같은 일반블록이거나
-            if(matrix[nr][nc] == R || matrix[nr][nc] == val){
-                q.push({nr,nc});
-                v[nr][nc] = 1;
+            queue<pos> q;
+            vector<pair<int,int>> group;
+            int color = matrix[i][j];
+            int rcnt = 0;
+            v[i][j] = 1;
+            q.push({i,j}); group.push_back({i,j});
+            
+            while(!q.empty()){
+                pos cur = q.front();
+                q.pop();
+                
+                for(int dir=0; dir<4; dir++){
+                    int nr = cur.i + dr[dir]; int nc = cur.j + dc[dir];
+                    tmp = {nr,nc};
+                    if(v[nr][nc] == 1) continue;
+                    if(find(group.begin(), group.end(), tmp) != group.end()) continue;
+                    
+                    if(matrix[nr][nc] == color || matrix[nr][nc] == R){
+                        q.push({nr,nc});
+                        group.push_back({nr,nc});
+                        v[nr][nc] = 1;
+                        
+                        if(matrix[nr][nc] == R) { v[nr][nc] = 0; rcnt++;}
+                    }
+                }
             }
+            
+            if(group_max.size() < group.size() || ( group_max.size() == group.size() && rcnt_max <= rcnt) ) {
+                group_max = group;
+                rcnt_max = rcnt;
+            }
+            group.clear();
+
         }
     }
+    
+    return group_max;
 }
 
 
 void solve(){
     
-    
     while(1){
-        // [1] 크기가 가장 큰 블록그룹 찾기
-        int cnt_max = 2;  // 그룹에 속한 블록의 개수는 2보다 크거나 같아야 하며.
-        int rcnt_max = 0;
-        group.clear();
+        // [1] 크기가 가장 큰 블록그룹 찾기.
+        // 무조건 미방문 일반 색상일때만 bfs하는 걸로 하자.
+        del_group = bfs();
+        if(del_group.size() < 2) break;
         
-        memset(v, 0, sizeof(v));
-        for(int i=1; i<=N; i++){
-            for(int j=1; j<=N; j++){
-                if(matrix[i][j] < 1) continue;
-                //if(v[i][j] == 1) continue;
-                if(matrix[i][j] == R) continue;
-                
-                // 무조건 미방문 일반 색상일때만 bfs하는 걸로 하자.
-                cnt = 0; rcnt = 0;
-                bfs(i,j);
-                if(cnt_max <= cnt){
-                    cnt_max = cnt;
-                    //cout << "group push_back " << i << j << endl;
-                    group.push_back({i,j, cnt, rcnt});
-                    if(rcnt_max <= rcnt) rcnt_max = rcnt;
-                }
-                
-                for(int i=1; i<=N; i++){
-                    for(int j=1; j<=N; j++){
-                        if(v[i][j] == 1 && matrix[i][j] == R) v[i][j] = 0;
-                    }
-                }
-            }
-        }
-        //cout << "cnt_max : " << cnt_max << endl;
-        // 탈출 조건.
-        if(cnt == 0 || group.size() == 0) break;
-        
-        vector<pair<int,int>> tg;
-        vector<pair<int,int>> rc;
-        //int max_i = 0; int max_j = 0;
-        int r = 0; int c = 0;
-//        for(auto n : group){
-//            if(n.cnt == cnt_max){
-//
-//                cout << "tg push_back " << n.i << " " << n.j << endl;
-//                tg.push_back({n.i, n.j});
-//            }
-//        }
-        //if(tg.size() == 0) cout << "tg사이즈0"<< endl;
-//        if(tg.size() != 0) {
-//            sort(tg.begin(), tg.end(), comp);
-//            r = tg[0].first;
-//            c = tg[0].second;
-//        }
-//        tg.clear();
-        
-        sort(group.begin(), group.end(), comp);
-        r = group[0].i;
-        c = group[0].j;
-        //cout << " r c " << r << " " << c << endl;
+        ans += del_group.size() * del_group.size();
         
         // [2] 해당 그룹 블록 clear
-        memset(v, 0, sizeof(v));
-        ans += cnt_max * cnt_max;
-        bfs_clear(r,c, 1);
-        
-//        cout << "clear 후" << endl;
-//        for(int i=1; i<=N; i++){
-//            for(int j=1; j<=N; j++){
-//                cout << matrix[i][j] << " " ;
-//            }
-//            cout << endl;
-//        }
-        
-        //cout << "ans : " << ans << "\n";
-        //cout << " r c " << r << " " << c << "\n";
-        
-        
-        // [3] 첫번째 중력 작용
-        for(int j=1; j<=N; j++){
-            for(int i=N-1; i>=1; i--){
-                if(matrix[i][j] < 1) continue;
-                
-                //cout << "i j 가 내려간다 : " << i << " " << j << endl;
-                for(int bottom = i+1; bottom <= N+1; bottom++){
-                    //cout << bottom << endl;
-                    if(matrix[bottom][j] != 0) {
-                        //cout << "여기에서 0아닌 걸 발견" << bottom-1 << endl;
-                        matrix[bottom-1][j] = matrix[i][j];
-                        if(bottom - 1 != i) matrix[i][j] = 0;
-                        break;
-                    }
-                }
-            }
+        for(auto cur : del_group){
+            matrix[cur.first][cur.second] = 0;
         }
         
-//        cout << "중력 후" << endl;
-//        for(int i=1; i<=N; i++){
-//            for(int j=1; j<=N; j++){
-//                cout << matrix[i][j] << " ";
-//            }
-//            cout << "\n";
-//        }
+        // [3] 첫번째 중력 작용
+        gravity();
         
         // [4] 90도 반시계 회전
         memset(tmp, 0, sizeof(tmp));
@@ -232,33 +137,8 @@ void solve(){
             }
         }
         
-        
         // [5] 마지막 두번째 중력 작용
-        for(int j=1; j<=N; j++){
-            for(int i=N-1; i>=1; i--){
-                if(matrix[i][j] < 1) continue;
-                
-                //cout << "i j 가 내려간다 : " << i << " " << j << endl;
-                for(int bottom = i+1; bottom <= N+1; bottom++){
-                    //cout << bottom << endl;
-                    if(matrix[bottom][j] != 0) {
-                        //cout << "여기에서 0아닌 걸 발견" << bottom-1 << endl;
-                        matrix[bottom-1][j] = matrix[i][j];
-                        if(bottom - 1 != i) matrix[i][j] = 0;
-                        break;
-                    }
-                }
-            }
-        }
-        
-//        cout << endl;
-//        cout << "마지막 중력 작용 후" << endl;
-//        for(int i=1; i<=N; i++){
-//            for(int j=1; j<=N; j++){
-//                cout << matrix[i][j] << " ";
-//            }
-//            cout << "\n";
-//        }
+        gravity();
         
     }
     cout << ans <<"\n";
