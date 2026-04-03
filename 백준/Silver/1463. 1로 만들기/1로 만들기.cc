399fd6cc6861aa55e67167a23b3413838523b524
argan719 <64789601+argan719@users.noreply.github.com>
2026-04-03 17:48:06 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,24 +1,53 @@
+// 1463 BFS
 #include<iostream>
+#include<queue>
+#include<cstring>
+#include<vector>
 #define MAX (int)(1e6 + 1)
 using namespace std;
 
 int N;
-int dp[MAX];
+vector<int> nums;
+int v[MAX];
 
+int bfs(){ // N -> 1
+    queue<int> q;
+    q.push(N);
+    v[N] = 1;
+    
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        
+        // 정답처리는 이곳에서
+        if(cur == 1) return v[1] - 1;
+        
+        if(cur % 3 == 0 && v[cur/3] == 0){
+            v[cur/3] = v[cur] + 1;
+            q.push(cur/3);
+        }
+        
+        if(cur % 2 == 0 && v[cur/2] == 0){
+            v[cur/2] = v[cur] + 1;
+            q.push(cur/2);
+        }
+        
+        if(cur - 1 > 0 && v[cur-1] == 0){
+            v[cur-1] = v[cur] + 1;
+            q.push(cur-1);
+        }
+    }
+    // 이곳에 올일은 없지만
+    return -1;
+}
 
 void solve(){
-    dp[1] = 0;
-    
-    for(int i=2; i<=N; i++){
-        dp[i] = dp[i-1] + 1;
-        if(i % 2 == 0) dp[i] = min(dp[i], dp[i/2]+1);
-        if(i % 3 == 0) dp[i] = min(dp[i], dp[i/3]+1);
-    }
+    int ans = bfs();
+    cout << ans;
 }
 
 int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     cin >> N;
     solve();
-    cout << dp[N];
 }
