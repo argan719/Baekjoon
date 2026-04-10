b0f491a09c6cf473c1f453fbf9d6123f20702e6c
argan719 <64789601+argan719@users.noreply.github.com>
2026-04-10 12:51:53 +0900

    [Platinum V] Title: 마법사 상어와 블리자드, Time: 4 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Platinum/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.cc
+++ 백준/Platinum/21611. 마법사 상어와 블리자드/마법사 상어와 블리자드.cc
@@ -1,159 +1,155 @@
 #include<iostream>
 #include<cstring>
-#include<vector>
 #include<algorithm>
+#include<vector>
 #define MAX 50
 using namespace std;
 
 int N, M;
 int matrix[MAX][MAX];
-//int tmp[MAX][MAX];
-int transform[MAX][MAX];
 
 struct Magic{
-    int d;
-    int s;
+    int d, s;
 };
-vector<Magic> magic;
-int lookup[5] = {0, 0, 2, 1, 3}; // 1,2,3,4 로 방향 정보가 들어오면 변환해줌.
-
-int cr, cc;  // 상어 있는 좌표
-// 상 좌 하 우
-int dr[] = {-1, 0, 1, 0};
-int dc[] = {0, -1, 0, 1};
 
 struct Pos{
-    int r, c;
+    int r,c;
 };
+
+vector<Magic> magic;
 vector<Pos> pos;
 
-//struct Marble{
-//    int r, c;
-//    int num; // 해당 구슬의 번호.
-//};
+Pos shark;
 
-//int ans1, ans2, ans3;
-int ans[4]; // 폭발한 1번, 2번, 3번 구슬 개수
+int ans[4];  // [1] = 폭발한 1번 구슬의 개수, [2] = 폭발한 2번 구슬의 개수, [3] = ""
+
+int dr[] = {0, 1, 0, -1};
+int dc[] = {-1, 0, 1, 0};
 
 void input(){
     cin >> N >> M;
-    Magic tmp;
-    cr = N/2;
-    cc = N/2;
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
             cin >> matrix[i][j];
         }
     }
+    int d, s;
     for(int i=0; i<M; i++){
-        cin >> tmp.d >> tmp.s;
-        tmp.d = lookup[tmp.d];
-        magic.push_back(tmp);
+        cin >> d >> s;
+        magic.push_back({d,s});
     }
     
-    // 나선형 좌표. 상어 다음 좌표부터 시작해서 0,0까지 미리 담아놓기.
-    int r = cr; int c = cc;
-    int flag = 0; int cnt = 0; int len = 1; int d = 1;
+    int cnt = 0; int flag = 0; int len = 1;
+    shark.r = N/2; shark.c = N/2;
+    int r = N/2; int c = N/2;
+    d = 0;
+    
     while(1){
-        r = r + dr[d]; c = c + dc[d]; cnt++;
+        int nr = r + dr[d]; int nc = c + dc[d];
+        pos.push_back({nr, nc});
+        //cout << nr << " " << nc << endl;
+        cnt++;
+        
         if(cnt == len){
-            if(flag == 0) {cnt = 0; flag = 1;}
-            else if(flag == 1) {cnt = 0; flag = 0; len++;}
-            d = (d+1) %4;
+            cnt = 0;
+            d = (d+1)%4;
+            
+            if(flag == 0) flag = 1;
+            else{
+                len++; flag = 0;
+            }
         }
-        pos.push_back({r, c});
-        if(r == 0 && c == 0) break;
+        
+        r = nr; c = nc;
+        
+        if(nr == 0 && nc == 0) break;
     }
-    
+
 }
 
-
 void solve(){
-    vector<int> marble;
-    vector<int> new_marble;
-    vector<int> transform;
     
-    for(int m=0; m<magic.size(); m++){
-        // [1] 파괴 - 블리자드 마법 시전
-        for(int mul=1; mul<=magic[m].s; mul++){
-            int nr = cr + dr[magic[m].d] * mul;
-            int nc = cc + dc[magic[m].d] * mul;
+    for(Magic m : magic){
+        if(m.d == 3) m.d = 0;
+        else if(m.d == 2) m.d = 1;
+        else if(m.d == 4) m.d = 2;
+        else if(m.d == 1) m.d = 3;
+        
+        
+        //[1] 구슬 파괴
+        for(int mul = 1; mul <= m.s; mul++){
+            int nr = shark.r + dr[m.d] * mul;
+            int nc = shark.c + dc[m.d] * mul;
             
-            if(nr < 0 || nr >= N || nc < 0 || nc >=N) continue;
+            if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
             matrix[nr][nc] = 0;
         }
         
-        // pos 순서로 matrix를 돌면서 0이 아닌 구슬들만 모은다.
+        // 구슬 이동 <-> 폭발 반복: 4개 이상 연속하는 구슬이 없을 때까지
+        vector<int> marble;
         for(auto n : pos){
-            if(matrix[n.r][n.c] > 0) marble.push_back(matrix[n.r][n.c]);
+            if(matrix[n.r][n.c] != 0) marble.push_back(matrix[n.r][n.c]);
         }
+        marble.push_back(-1);
         
-        // [2] - [3] 이동과 폭발 반복.
-        // marble을 가지고 4개 연속 확인 후
-        // explode에 4개 연속을 전부 지운 구슬들을 넣고 그걸 다시 marble에 넣고 .. 반복.
-        int flag = 0;
         while(1){
-            // 맨 마지막에 패딩으로 -1을 넣어준다.
-            marble.push_back(-1);
-            new_marble.clear();
-            flag = 0;
+            vector<int> tmp;
+            int flag = 0;
             
-            int s = 0; int e = 0;
-            while(s < marble.size() - 1){
-                e = s + 1;
-                while(marble[s] == marble[e]){
-                    e++;
-                }
-                if(e - s >= 4) {
-                    flag = 1;
-                    // 정답처리
-                    for(int i=s; i<e; i++) {
-                        ans[marble[i]]++;
+            int s = 0; int cnt = 0;
+            for(int e=0; e<marble.size(); e++){
+                
+                if(marble[s] != marble[e]){
+                    if(e - s < 4) {
+                        while(s != e) {
+                            tmp.push_back(marble[s]); s++;
+                        }
+                    }
+                    else if(e - s >=4) {
+                        flag = 1;
+                        while(s != e) {
+                            ans[marble[s]]++; s++;
+                        }
                     }
                 }
-                else if(e - s < 4){
-                    for(int i=s; i<e; i++) new_marble.push_back(marble[i]);
-                }
-                s = e;
             }
-            marble.clear();  // -1이 있으므로 걍 클리어 후 새로 대입.
-            marble = new_marble;
-            //marble.push_back(-1);
-            //new_marble.clear();  // 이것도 반드시 클리어 필요.
+            marble.clear();
+            marble = tmp;
+            tmp.clear();
+            marble.push_back(-1);
             
-            //cout << flag << "\n";
-            // 4개 연속이 없었다면 더 이상 반복하지 않으므로 탈출.
             if(flag == 0) break;
         }
         
-        // [4] 변화
-        // marble을 가지고 A, B를 구해서 matrix를 새롭게 만들어야 하는 것
-        marble.push_back(-1);
         
-        int s = 0; int e = 0;
-        while(s < marble.size() - 1){
-            e = s + 1;
-            while(marble[s] == marble[e]){
-                e++;
-            }
-            transform.push_back(e-s);  // A삽입
-            transform.push_back(marble[s]); // B삽입
-            s = e;
-        }
-        
-        while(transform.size() < pos.size()){
-            transform.push_back(0);  // 0으로 동일하게 채워줘서 pos 기준으로 matrix에 채워넣기.
-        }
-        
+        //[4] 구슬 변화
         memset(matrix, 0, sizeof(matrix));
-        for(int i=0; i<pos.size(); i++){
-            matrix[pos[i].r][pos[i].c] = transform[i];
+        vector<int> tmp;
+        int s = 0; int cnt = 0;
+        for(int e=0; e<marble.size(); e++){
+            if(marble[s] != marble[e]){
+                tmp.push_back(e-s);
+                tmp.push_back(marble[s]);
+                s = e;
+            }
         }
-        transform.clear();
-        marble.clear();
+        
+        int size = min(tmp.size(), pos.size());
+        for(int i=0; i<size; i++){
+            matrix[pos[i].r][pos[i].c] = tmp[i];
+        }
+        
+        
+//        cout << "구슬 변화 확인 \n";
+//        for(int i=0; i<N; i++){
+//            for(int j=0; j<N; j++){
+//                cout << matrix[i][j] << " " ;
+//            }
+//            cout << endl;
+//        }
+        
     }
-    
-    
+    cout << 1* ans[1] + 2*ans[2] + 3*ans[3];
 }
 
 
@@ -161,5 +157,4 @@ int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
     solve();
-    cout << (1 * ans[1]) + (2 * ans[2]) + (3 * ans[3]) << "\n";
 }
