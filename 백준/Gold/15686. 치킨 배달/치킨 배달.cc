a725f488ea5ba43346410b3448f764a2a443d77c
argan719 <64789601+argan719@users.noreply.github.com>
2026-04-17 09:44:29 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,62 +1,55 @@
 #include<iostream>
-#include<vector>
 #include<cstring>
+#include<vector>
 #include<algorithm>
 #define MAX 51
 using namespace std;
 
 int N, M;
-int matrix[MAX][MAX];
-int v[MAX];
+vector<pair<int,int>> house;
+vector<pair<int,int>> chicken;
 
-struct Pos{
-    int r, c;
-};
-vector<Pos> house;
-vector<Pos> chicken;
-
-int mlst[15]; // 선정한 치킨집. 0~M-1
-int ans = 1e5;
+int arr[15];
+int ans = 50000;
 
 void input(){
     cin >> N >> M;
-    for(int i=1; i<=N; i++){
-        for(int j=1; j<=N; j++){
-            cin >> matrix[i][j];
-            if(matrix[i][j] == 0) continue;
-            if(matrix[i][j] == 1) house.push_back({i,j});  // 집 좌표
-            else if(matrix[i][j] == 2) chicken.push_back({i,j}); // 치킨집 좌표
+    int val;
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            cin >> val;
+            if(val == 1) house.push_back({i,j});
+            else if(val == 2) chicken.push_back({i,j});
         }
     }
 }
 
 void dfs(int n, int start){
-    // 종료조건은 이곳에서
     if(n == M){
-        // 치킨거리의 합 구해서 최솟값 갱신.
+        // 정답처리
         int sum = 0;
         for(auto h : house){
-            int house_min = 1000;
-            for(int i=0; i<M; i++){
-                house_min = min(house_min, abs(chicken[mlst[i]].r - h.r) + abs(chicken[mlst[i]].c - h.c));
+            int min_dist = 300;
+            for(int c=0; c<chicken.size(); c++){
+                if(arr[c] == 0) continue;
+                
+                min_dist = min(min_dist, abs(chicken[c].first - h.first) + abs(chicken[c].second - h.second));
             }
-            sum += house_min;
+            sum += min_dist;
         }
         ans = min(ans, sum);
+        
         return;
     }
     
     for(int i=start; i<chicken.size(); i++){
-        //if(v[i]) continue;
-        //v[i] = 1;
-        mlst[n] = i;
+        arr[i] = 1;
         dfs(n+1, i+1);
-        //v[i] = 0;
+        arr[i] = 0;
     }
 }
 
-
-int main(void){
+int main(){
     ios::sync_with_stdio(false); cin.tie(0);
     input();
     dfs(0, 0);
